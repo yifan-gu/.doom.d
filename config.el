e97f9cdb5f5fbdc4b839d08c1d66dfa2595a101e
Yifan <guyifan1121@gmail.com>
2022-02-07 00:57:09 -0800

    Init commit

---
+++ config.el
@@ -0,0 +1,231 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Yifan Gu"
+      user-mail-address "guyifan1121@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Monaco" :size 16 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Monaco" :size 16))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+;;
+
+
+;;
+;; ==== UI/UX ====
+;;
+
+(defun transparency (value)
+  "Sets the transparency of the frame window. 0=transparent/100=opaque"
+  (interactive "nTransparency Value 0 - 100 opaque:")
+  (set-frame-parameter (selected-frame) 'alpha value))
+
+(set-frame-parameter (selected-frame) 'alpha 100)
+
+;; metakey for macos
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier 'none)
+
+(setq echo-keystrokes 0.1)
+
+(setq scroll-margin 3
+      scroll-conservatively 10000)
+(setq scroll-step 0)
+
+;; move 4 line
+(global-set-key "\M-n"  (lambda () (interactive) (next-line   4)) )
+(global-set-key "\M-p"  (lambda () (interactive) (previous-line 4)) )
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; quick shortcut for recet open file
+;; (setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; bind window move key
+(global-set-key "\M-k" 'windmove-up)
+(global-set-key "\M-j" 'windmove-down)
+(global-set-key "\M-h" 'windmove-left)
+(global-set-key "\M-l" 'windmove-right)
+
+(global-set-key "\M-\S-k" 'buf-move-up)
+(global-set-key "\M-\S-j" 'buf-move-down)
+(global-set-key "\M-\S-h" 'buf-move-left)
+(global-set-key "\M-\S-l" 'buf-move-right)
+
+;; behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+  See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
+
+;; behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+  See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-o") 'open-previous-line)
+
+;; autoindent open-*-lines
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+
+;; override DOOM emacs "C-a" and "C-e"
+(global-set-key (kbd "C-a") 'move-beginning-of-line)
+(global-set-key (kbd "C-e") 'move-end-of-line)
+
+;; disable smartparens mode
+(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
+
+;; open recentf in new window
+(defun open-recentf-in-new-window ()
+  (interactive)
+  (if (= (length (window-list)) 1)
+      (split-window-horizontally))
+  (other-window 1)
+  (recentf-open-files))
+
+(global-set-key (kbd "C-x 4 C-r") 'open-recentf-in-new-window)
+
+;; avy jump
+;;(define-key global-map (kbd "C-c SPC") 'avy-goto-char)
+(global-set-key (kbd "C-c SPC") 'avy-goto-char)
+
+
+;;
+;; ==== lang ====
+;;
+
+;; go mode
+(gofmt-before-save)
+(add-hook 'go-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-*") 'pop-tag-mark)))
+
+;; org
+(setq +org-roam-open-buffer-on-find-file nil)
+
+(defun org-open-link-in-other-window ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1)
+  (org-open-at-point))
+
+(defun org-meta-return-with-store-link ()
+  (interactive)
+  (org-meta-return)
+  (org-id-store-link))
+
+;; Create roam links for all level 1 headers.
+(defun org-create-links-for-all-headers ()
+  (org-map-entries (lambda () (org-id-store-link) "LEVEL=1")))
+
+;; Create roam links for the title plus all level 1 headers.
+(defun org-create-all-links ()
+  (interactive)
+  (beginning-of-buffer)
+  (org-id-store-link)
+  (org-create-links-for-all-headers))
+
+(defun load-org-theme()
+  (interactive)
+  (load-theme 'doom-homage-white))
+
+(defun load-default-theme()
+  (interactive)
+  (load-theme 'doom-one))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (local-set-key (kbd "M-*") 'org-mark-ring-goto)
+            (local-set-key (kbd "C-c C-j") 'org-open-at-point)
+            (local-set-key (kbd "C-x 4 C-c C-j") 'org-open-link-in-other-window)
+            (local-set-key (kbd "C-c <C-return>") 'org-meta-return-with-store-link)))
+
+(org-roam-db-autosync-mode)
+
+;; roam-ui
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
+;; deft
+(setq deft-directory "~/org"
+      deft-default-extension "org"
+      deft-use-filter-string-for-filename t
+      deft-recursive t)
+
+
+;; TODO
+;; highlight, grammar check
+;; jsonnet?
+;;
---
+++ custom.el
@@ -0,0 +1,53 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(custom-safe-themes
+   '("97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(exwm-floating-border-color "#191b20")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors
+   ((("#333a38" "#99bb66" "green")
+     . 0)
+    (("#2b3d48" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(objed-cursor-color "#ff6c6b")
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
+ '(rustic-ansi-faces
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(vc-annotate-background "#282c34")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
